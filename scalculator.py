be90c273d7befaa75f3be3a37387cd5a1e4cc32e
Dr. Eric O. Flores <eric@florestoro.com>
2024-09-14 09:29:45 -0600

    Update scalculator.py

--- scalculator.py
+++ scalculator.py
@@ -1,22 +1,22 @@
-/*
-GPL3 License Notice
-EfCalc Pro - A Python-based Scientific Calculator
-
-Copyright (C) 2024 Dr. Eric O. Flores – E-mail:  <eoftoro@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
-*/
+#
+# GPL3 License Notice
+# EfCalc Pro - A Python-based Scientific Calculator
+#
+# Copyright (C) 2024 Dr. Eric O. Flores – E-mail:  <eoftoro@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
 
 import sys
 import math
